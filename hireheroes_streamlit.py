805cf7101df5d9110c206b95eb1130939c58a28a
Shumaila <55207354+shumailaalid@users.noreply.github.com>
2025-05-09 03:58:12 +0500

    Update hireheroes_streamlit.py

--- hireheroes_streamlit.py
+++ hireheroes_streamlit.py
@@ -4,6 +4,7 @@ import pandas as pd
 from pathlib import Path
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time, re, csv
